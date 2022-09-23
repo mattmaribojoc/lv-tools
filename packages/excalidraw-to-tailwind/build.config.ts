6494febf035be59454042dae9333b1ade5185263
Matt Maribojoc <matthewmaribojoc@gmail.com>
2022-09-22 23:05:13 -0400

    update build

--- packages/excalidraw-to-tailwind/build.config.ts
+++ packages/excalidraw-to-tailwind/build.config.ts
@@ -2,8 +2,6 @@ import { defineBuildConfig } from 'unbuild'
 
 export default defineBuildConfig({
   declaration: true,
-  rollup: {
-    inlineDependencies: true
-  },
-  entries: ['src/cli', 'src/index']
+  entries: ['src/cli', 'src/index'],
+  externals: ['mri', 'readline', 'svgo', 'zod']
 })
